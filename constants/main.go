424cb608d299c02701ec9bf6655137736f1446a5
Nicholas Perry <nicholasperrypro@gmail.com>
2026-04-29 11:45:39 -0400

    docker compose, mqtt to kafka bridge

--- constants/main.go
+++ constants/main.go
@@ -3,5 +3,5 @@ package constants
 var (
 	DeviceEventsTopic = "devices"
 	EventTypes        = []string{"temperature", "humidity", "pressure", "battery"}
-	DeviceIDs         = []string{"dev-001", "dev-002", "dev-003", "dev-004", "dev-005"}
+	DeviceIDs         = []string{"dev-001", "dev-002", "dev-003", "dev-004", "dev-005", "dev-006", "dev-007", "dev-008", "dev-009", "dev-010"}
 )
